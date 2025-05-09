56510d968554a9baec7e5b9983ba35bb77c0e07c
dipankor99 <dipankor.gratia@gmail.com>
2025-05-09 23:07:33 +0530

    add my photo

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,21 +1,42 @@
+import { motion } from "framer-motion";
 
-const heading ={
-    image: ''
-}
+const heading = {
+  image: "/my-photo.png",
+};
 
-export const HeroSection =()=>{
+export const HeroSection = () => {
+  return (
+    <div className="w-full h-full pt-20 pb-60" id="home">
+      <div className="flex flex-col md:flex-row items-center md:items-start justify-center md:justify-start max-w-6xl mx-auto px-4 gap-8">
 
-    return (
-        <>
-            <div className="border w-full h-full top-15 pt-60 pb-60" id="home">
-                <div className="flex justify-center">
-                    <img src={heading.image} alt="" srcset="" height={100} width={100}/>
-                </div>
-                <div>
-                        <h2>Lorem ipsum dolor sit.</h2>
-                        <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur, molestiae!</h1>
-                    </div>
-            </div>
-        </>
-    )
-}
+        {/* Animated Image Section - Slide from Left */}
+        <motion.div
+          initial={{ x: -200, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+          className="flex justify-center md:justify-start w-full md:w-1/2"
+        >
+          <img
+            src={heading.image}
+            alt="Profile"
+            className="rounded-[70%_30%_30%_70%] w-48 h-48 sm:w-64 sm:h-64 md:w-80 md:h-80 object-cover transition-all"
+          />
+        </motion.div>
+
+        {/* Animated Text Section - Slide from Right */}
+        <motion.div
+          initial={{ x: 200, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.8, ease: "easeOut", delay: 0.2 }}
+          className="w-full md:w-1/2 text-center md:text-left"
+        >
+          <h2 className="text-lg font-semibold mb-2">Lorem ipsum dolor sit.</h2>
+          <h1 className="text-2xl md:text-4xl font-bold">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur, molestiae!
+          </h1>
+        </motion.div>
+
+      </div>
+    </div>
+  );
+};
